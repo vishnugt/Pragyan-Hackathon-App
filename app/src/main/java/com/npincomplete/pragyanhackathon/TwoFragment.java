fe4fd68f35aaac7c81b90938704d19a1ceeab6a7
Meliodas <vishnugt@hotmail.com>
2016-12-10 00:06:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/npincomplete/pragyanhackathon/TwoFragment.java
@@ -0,0 +1,31 @@
+package com.npincomplete.pragyanhackathon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Meliodas on 09/12/2016.
+ */
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+}
